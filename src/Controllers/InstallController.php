959481885333558d2658c9d853dce0fff7bfcadd
Thiago Natanael <thiago_natanael@msn.com>
2019-01-11 14:25:32 -0200

    Changed User Model Location

--- src/Controllers/InstallController.php
+++ src/Controllers/InstallController.php
@@ -3,7 +3,7 @@
 namespace Kordy\Ticketit\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\User;
+use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
--- src/Models/Agent.php
+++ src/Models/Agent.php
@@ -2,7 +2,7 @@
 
 namespace Kordy\Ticketit\Models;
 
-use App\User;
+use App\Models\User;
 use Auth;
 
 class Agent extends User
